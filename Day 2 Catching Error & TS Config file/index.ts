c8ffaec71668f7582a082d3af21ec7d5c5dcbb0d
Himanshu <himanshuparodiya@gmail.com>
2023-09-29 14:21:15 +0530

    Intro to Tsconfig file

---
+++ Day 2 Catching Error & TS Config file/index.ts
@@ -0,0 +1,12 @@
+//- TODO 1: Write a program to add two numbers
+
+//- TODO 2: How to catch Errors & solve it?
+
+//- TODO 3: TS Configuration file
+
+//@ts-ignore
+
+function addTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+addTwoNumbers(5, 'h');
